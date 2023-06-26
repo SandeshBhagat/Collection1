97ec5589b83e66fc779b93caec0293cef0c0c4ed
sandesh bhagat <sandesh bhagat@ho.rnt.ai>
2023-06-26 17:09:34 +0530

    new changes

--- Projects/React/adventure-book/frontend/src/components/AllAdventures.js
+++ Projects/React/adventure-book/frontend/src/components/AllAdventures.js
@@ -2,13 +2,20 @@ import "../css/allAdv.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import img from "../assets/img/login-bg.jpg";
+import { useNavigate } from "react-router-dom";
 
 const AllAdv = () => {
+  const history = useNavigate();
+
+  const navigate = () => {
+    history("/viewadv");
+  };
+
   return (
     <div className="alladv-main">
       <h2>All Adventures</h2>
       <div className="cards-container">
-        <div className="card">
+        <div className="card" onClick={navigate}>
           <img src={img} alt="" />
           <p className="title">This is card title</p>
           <p className="date">27/06/2023</p>
